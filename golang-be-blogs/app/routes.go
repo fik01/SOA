b2b076694cef5f065d721e64b61e7b63964a1d9d
Marko Radetic <markoradetic67@gmail.com>
2024-03-20 03:20:49 +0100

    feat: Added routes for endpoints.

--- golang-be-blogs/app/routes.go
+++ golang-be-blogs/app/routes.go
@@ -11,4 +11,7 @@ func Route(router *mux.Router, handler *handler.Handler) {
 	router.HandleFunc("/blog/createComment", handler.CommentHandler.Create).Methods("POST")
 	router.HandleFunc("/blog/updateComment", handler.CommentHandler.Update).Methods("PUT")
 	router.HandleFunc("/blog/deleteComment/{id}", handler.CommentHandler.Delete).Methods("DELETE")
+
+	router.HandleFunc("/blog/blogComments", handler.BlogHandler.GetAll).Methods("GET")
+	router.HandleFunc("/blog/createComment", handler.BlogHandler.Create).Methods("POST")
 }
