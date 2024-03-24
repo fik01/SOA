fe3765c84f69385a90745200e4743281d66e869c
Σ <mboskovic420@gmail.com>
2024-03-24 21:46:26 +0100

    added:
    - dockerized go tours service
    - dockerized .net monolith api
    - added docker-compose file for building the monolith container with the go tour container and the postgre db container

--- golang-be/Tours/config/config.go
+++ golang-be/Tours/config/config.go
@@ -1,7 +1,15 @@
 package config
 
+import "os"
+
 func GetConnectionString() string {
-	return "host=localhost user=postgres password=super dbname=test_tour port=5432 sslmode=disable"
+	connectionString, isPresent := os.LookupEnv("DATABASE_URL")
+	if isPresent {
+		return connectionString
+	} else {
+		return "host=localhost user=postgres password=super dbname=test_tour port=5432 sslmode=disable"
+	}
+
 }
 
 func GetPort() string {
