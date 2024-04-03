7168e7f8eb72e69ed0c6c6352d4216f8f441c0eb
Marko Radetic <markoradetic67@gmail.com>
2024-04-03 14:25:30 +0200

    feat: Changing url for db based on docker.

--- golang-be-blogs/config/config.go
+++ golang-be-blogs/config/config.go
@@ -1,7 +1,14 @@
 package config
 
+import "os"
+
 func GetConnectionString() string {
-	return "host=localhost user=postgres password=super dbname=Blogs port=5432 sslmode=disable"
+	connectionString, isPresent := os.LookupEnv("DATABASE_URL2")
+	if isPresent {
+		return connectionString
+	} else {
+		return "host=localhost user=postgres password=super dbname= port=5432 sslmode=disable"
+	}
 }
 
 func GetPort() string {
